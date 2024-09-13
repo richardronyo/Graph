26f84a2bebec8727df7d1758542b7445fa4d5ccc
richardronyo <richardronyo@gmail.com>
2024-09-13 12:17:51 -0600

    Can now create visualizations of the graphs with visualize() method. Can also generate a complete graph

--- graph.py
+++ graph.py
@@ -1,32 +1,89 @@
 from node import Node
-
+import matplotlib.pyplot as plt
+import math
 
 class Graph:
-    def __init__(self, n):
+    """
+    This implementation of a Graph uses a list to store it's nodes instead of pointers. This is reflective of using an adjacency list.
+    Nodes are numbered in a counter-clockwise manner, in which it starts with the node located at 0 radians on the unit circle.
+
+    Attributes:
+        nodes -> [] of Node objects
+        n -> Number of nodes
+        data -> A list of the data belonging to each Node
+    """
+    def __init__(self, n, data = None):
         self.nodes = []
         self.n = 0
 
         for i in range(n):
-            self.nodes.append(Node(i))
+            if data == None:
+                self.nodes.append(Node(i))
+            else:
+                self.nodes.append(Node(i, data[i]))
             self.n += 1
 
-    def edge(self, node1, node2):
-        self.nodes[node1].edges.append(node2)
-        self.nodes[node2].edges.append(node1)
-    
     def __str__(self):
         graph_str = f""
 
         for i in range(self.n):
-            graph_str += f"Node {i}:\n\t{self.nodes[i]}\n"
+            graph_str += f"Node {i + 1}:\n\t{self.nodes[i]}\n"
 
         return graph_str
     
+    def edge(self, pos1, pos2):
+        self.nodes[pos1].edges.append(pos2)
+        self.nodes[pos2].edges.append(pos1)
+
+    def complete(self):
+        if self.n > 1:
+            for i in range(self.n):
+                for j in [index for index in range(self.n) if index != i]:
+                    self.edge(i, j)
+                
+    def compute_coord(self, radius = 1):
+        coord = []
+
+        for i in range(self.n):
+            x = radius * math.cos(2 * math.pi * i / self.n)
+            y = radius * math.sin(2 * math.pi * i / self.n)
+
+            coord.append((x, y))
+
+        return coord
+    
+    def visualize(self):
+        fig, ax = plt.subplots()
+
+        coord = self.compute_coord()
+
+        #This portion of code draws the edges
+        for node in self.nodes:
+            for edge in node.edges:
+                x_values = [coord[node.pos][0], coord[edge][0]]
+                y_values = [coord[node.pos][1], coord[edge][1]]
+                ax.plot(x_values, y_values, 'k-', lw=1)
+
+        # Plot nodes
+        for node in self.nodes:
+            ax.plot(coord[node.pos][0], coord[node.pos][1], 'bo', markersize=10)  # Draw nodes as blue circles
+            ax.text(coord[node.pos][0] + 0.05, coord[node.pos][1] + 0.05, str(node.pos + 1), fontsize=12)  # Label nodes
+
+        # Set equal aspect ratio and limits to make the graph look proportional
+        ax.set_aspect('equal')
+        ax.set_xlim(-1.5, 1.5)
+        ax.set_ylim(-1.5, 1.5)
+
+        # Add a title and show the plot
+        plt.title(f"{self.n}-gon Graph")
+        plt.show()
+
+    
+
+
 
 if __name__ == "__main__":
-    graph = Graph(5)
-
     for i in range(5):
-        graph.edge(i, 4 - i)
-
-    print(graph)
+        ki = Graph(i + 1)
+        ki.complete()
+        ki.visualize()
--- node.py
+++ node.py
@@ -1,10 +1,22 @@
 class Node:
-    def __init__(self, data):
+    """
+    This implemetation of a Node uses an adjacency list format to list the edges. Instead of containing a pointer to the next node, this implementation contains a list of the indices of the end node in the overall Graph.
+    Nodes are numbered in a counter-clockwise manner, in which the node located at 0 radians is first. My implementation makes possible for the use of an adjacency list when used in building a graph.
+
+
+    Attributes:
+        pos -> int
+        data -> any data type Any kind of information desired to be stored
+        edges -> [] of int
+    """
+    
+    def __init__(self, pos, data = None):
+        self.pos = pos
         self.data = data
         self.edges = []
 
     def __str__(self):
-        return f"Data:{self.data}\n\tEdges:{self.edges}"
+        return f"Data: {self.data}\n\tEdges: {self.edges}"
 
 
 
